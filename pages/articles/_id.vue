f86d8bdc842865199f12013c6e99abbe1e19e0ac
Soichi Saito <soichi.saito@bizreach.co.jp>
2019-01-09 19:24:29 +0900

    Fix tag

--- pages/articles/_id.vue
+++ pages/articles/_id.vue
@@ -1,10 +1,8 @@
 <template>
   <div>
     <article>
-      <div>
-        <h1>{{ article.title }}</h1>
-        <p>{{ article.body }}</p>
-      </div>
+      <h1>{{ article.title }}</h1>
+      <p>{{ article.body }}</p>
     </article>
     <nuxt-link :to="{ name: 'articles' }">Back</nuxt-link>
   </div>
