f5007f574a22b763d873adedb42ad9e24cfc45de
Soichi Saito <soichi.saito@bizreach.co.jp>
2019-01-11 15:35:23 +0900

    Add game page

---
+++ pages/game/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="content"/>
+</template>
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
+<script>
+import axios from 'axios'
+import 'phaser'
+import TestScene from './scenes/PlayScene.js'
+
+const config = {
+  type: Phaser.AUTO,
+  parent: 'content',
+  width: 640,
+  height: 480,
+  resolution: 1,
+  backgroundColor: '#EDEEC9',
+  scene: [TestScene]
+}
+new Phaser.Game(config)
+
+export default {
+  async asyncData({ params }) {
+    let { data } = await axios.get(`${process.env.apiUrl}/articles.json`)
+    return { articles: data }
+  }
+}
+</script>
---
+++ pages/game/scenes/PlayScene.js
@@ -0,0 +1,45 @@
+import desertJson from '~/assets/tilemaps/desert.json'
+import desertImage from '~/assets/tilemaps/tmw_desert_spacing.png'
+import playerImage from '~/assets/sprites/mushroom.png'
+
+class TestScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'TestScene' })
+  }
+
+  preload() {
+    this.load.tilemapTiledJSON('map', desertJson)
+    this.load.image('Desert', desertImage)
+    this.load.image('player', playerImage)
+  }
+
+  create() {
+    const map = this.make.tilemap({ key: 'map' })
+    const tileset = map.addTilesetImage('Desert')
+    const layer = map.createStaticLayer(0, tileset, 0, 0)
+
+    this.player = this.add.sprite(100, 100, 'player')
+    this.cursors = this.input.keyboard.createCursorKeys()
+
+    this.cameras.main.setBounds(0, 0, map.withInPixels, map.heightInPixels)
+    this.cameras.main.startFollow(this.player, false)
+  }
+
+  update(time, delta) {
+    this.player.angle += 1
+    if (this.cursors.left.isDown) {
+      this.player.x -= 5
+    }
+    if (this.cursors.right.isDown) {
+      this.player.x += 5
+    }
+    if (this.cursors.down.isDown) {
+      this.player.y += 5
+    }
+    if (this.cursors.up.isDown) {
+      this.player.y -= 5
+    }
+  }
+}
+
+export default TestScene
